7025680b324c152f57c1e50dbef2067ec7bbbd60
Zer0356 <86683282+Zer0356@users.noreply.github.com>
2021-07-01 15:40:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ photo = ""
 def pr():
     def object_detection_on_an_image():
         segment_image = instance_segmentation()
-        segment_image.load_model('fruits_360_model.h5')
+        segment_image.load_model('mask_rcnn_coco.h5')
 
         segment_image.segmentImage(
             image_path = photo,
